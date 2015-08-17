d040c391b9c5f8e0ae9cb4a998aab2eb618a6976
Ethan Chiu <ethanc@Ethans-MacBook-Air.local>
2015-08-17 18:56:18 -0500

    Created value to make navbar collapse
    - created global var and set it to intro
    - deleted “50”
    - added global var as comparison in line 6

--- js/js.js
+++ js/js.js
@@ -1,12 +1,10 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+//get value of the intro so that we can use it for our collapsible navbar
+var introheight =  $('.intro').height();
+
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > introheight) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
